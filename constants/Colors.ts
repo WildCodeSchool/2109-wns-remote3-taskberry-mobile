eabce538b831370c74c796bcb8a5986dca30db35
Quentin <dev.mahjopi@mahjopi.com>
2022-02-18 11:34:52 +0100

    add component NotificationsCard and use in screen Notifications

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,25 +1,25 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    purple : '#4C1686',
-    darkPurple : '#011053',
-    lightGrey : '#F3F3F3',
-    tabIconDefault: '#ccc',
+    purple: "#4C1686",
+    darkPurple: "#011053",
+    lightGrey: "#F3F3F3",
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    purple : '#4C1686',
-    darkPurple : '#011053',
-    lightGrey : '#F3F3F3',
-    tabIconDefault: '#ccc',
+    purple: "#4C1686",
+    darkPurple: "#011053",
+    lightGrey: "#F3F3F3",
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
 };
