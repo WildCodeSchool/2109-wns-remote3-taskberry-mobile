6f01e9b4927966a850bd542a55468e210ef05664
brdtheo <theo.billardey@gmail.com>
2022-02-23 10:25:36 +0100

    import project context

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,36 +1,48 @@
-import { StyleSheet, ScrollView } from "react-native";
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-import Project from '../components/Project';
-import image from '../constants/Images';
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { RootTabScreenProps } from "../types";
+import Project from "../components/Project";
+import image from "../constants/Images";
+import { ProjectContext } from "../providers/ProjectProvider";
+import { useContext } from "react";
 
-export default function HomeScreen({ navigation }: RootTabScreenProps<'Projets'>) {
+export default function HomeScreen({
+  navigation,
+}: RootTabScreenProps<"Projets">) {
+  const { projects } = useContext(ProjectContext);
+
+  const projectItem = ({ item }: { item: Project }) => {
+    return (
+      <Project
+        title={item.name}
+        date={item.createdAt}
+        leader={image.avatar_4}
+      />
+    );
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.projectsWrapper}>
-        <Project
-          title={"Express projet"}
-          date={"10/12/2021"}
-          leader={image.avatar_4}
-        />
-        <Project
-          title={"React projet"}
-          date={"11/11/2022"}
-          leader={image.avatar_4}
-        />
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        data={projects}
+        renderItem={projectItem}
+        contentContainerStyle={styles.contentContainer}
+        overScrollMode="never"
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
   },
-  projectsWrapper: {
-    padding : 20,
+  contentContainer: {
+    marginHorizontal: 20,
+    marginTop: 12,
+    paddingVertical: 12,
   },
-  sectionTitle :{
-    fontSize : 24,
-  }
+  sectionTitle: {
+    fontSize: 24,
+  },
 });
