5d7ee902f4abe77bdcb52d7351e2ac05f520d322
brdtheo <theo.billardey@gmail.com>
2022-02-23 14:58:21 +0100

    update config

--- types.tsx
+++ types.tsx
@@ -28,6 +28,7 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
 export type RootTabParamList = {
   Projets: undefined;
   Notifications: undefined;
+  Tickets : undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
